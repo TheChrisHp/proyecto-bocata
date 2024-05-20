c1d2709d931f416f8b3521dd134d337049ab72c6
Christian Fortes <lucasesmid@gmail.com>
2024-05-20 20:50:39 -0300

    arreglos en el nav

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -20,7 +20,7 @@ export default function Demo() {
       <Carousel.Slide> <img src="https://res.cloudinary.com/dfnton7oq/image/upload/v1715971266/samples/bocata-imgs/438814323_828545982636308_1284979537341310745_n_oqhr8t_pv2o6o.jpg" style={{ width: '100%', height: '120%', objectFit: 'cover'}} alt="" /> 
       </Carousel.Slide>
 
-      <Carousel.Slide> <img src="https://res.cloudinary.com/dfnton7oq/image/upload/v1714697188/samples/bocata-imgs/img42_c9vsji.jpg" alt="" style={{ width: '100%', height: 'auto', objectFit: 'cover'}} /> </Carousel.Slide>
+      <Carousel.Slide> <img src="https://res.cloudinary.com/dfnton7oq/image/upload/v1714699305/samples/bocata-imgs/photo-1578916171728-46686eac8d58_r8vqa9.avif" alt="" style={{ width: '100%', height: '100%', objectFit: 'cover', filter: 'blur(2px)' }} /> </Carousel.Slide>
       </Carousel>
   );
 }
