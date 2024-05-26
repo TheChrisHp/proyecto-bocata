685c30eb47d4af2ab5b8bbff8e7c07c237951e36
Christian Fortes <lucasesmid@gmail.com>
2024-05-26 00:44:28 -0300

    prueba

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://thechrishp.github.io/proyecto-bocata/",
   plugins: [react()],
 })
